23837894e23a277a4ffed66a58e2c1e66f5e9998
Alexander Koik-Cestone <akoikcestone@gmail.com>
2021-10-09 11:11:23 -0400

    Change angle from string to number

--- src/app/components/compass/compass.component.ts
+++ src/app/components/compass/compass.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./compass.component.sass']
 })
 export class CompassComponent implements OnInit {
-  @Input() angle: string = '0';
+  @Input() angle: number = 0;
 
   constructor() {}
 
